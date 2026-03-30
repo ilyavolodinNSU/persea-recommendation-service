c8d64d14224a40482878b3bf5a6fb7d41b4878af
Ilya <i.volodin@g.nsu.ru>
2026-03-30 15:11:26 +0700

    init

---
+++ src/main/java/ru/persea/recommendationservice/PerseaRecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.persea.recommendationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerseaRecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerseaRecommendationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/persea/recommendationservice/PerseaRecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.persea.recommendationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerseaRecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
